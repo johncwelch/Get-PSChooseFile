6e915b3d92f7a8e95185088b408e5f5f06c3942a
John C. Welch <jwelch@bynkii.com>
2024-08-29 12:54:06 -0500

    upates

--- Get-ChooseFile.ps1
+++ Get-ChooseFile.ps1
@@ -59,3 +59,122 @@ function Get-ChooseFile {
      }
 
 }
+
+#use a custom class here so we don't have to deal with name collisions, which there will be many
+#this way we can just create an array of fileTypes and pass it to the check as an array
+#we build the initial array here, then use the filetypes, if any, to build an array to pass to the function
+
+class fileType {
+     [string]$name
+     [string]$type
+
+     fileType(
+          [string]$name,
+          [string]$type
+     ){
+          $this.name = $name
+          $this.type = $type
+     }
+}
+
+#arrayList to hold the filetypes
+[System.Collections.ArrayList]$fileTypeArrayList=@()
+
+#the order of the array is somewhat important, dealing with common special cases first
+$sourceFileTypeArray = @("jpg","jpeg","doc","docx","xls","xlsx","ppt","pptx","pdf","psd","indd","ai","gif","png","mpeg","mp3","mp4","m4a","aiff","heic","pages","key","keynote","numbers","epub","ibooks","rtf","applescript","scpt","scptd","script","sh","py","pl","ps1","url","zip","app","pxm","der","p7c","pem","crt","cer","txt","text","vcf","ics","html","htm","sql","webloc","plist","workflow","lz4","json","csv","tsv","sqlite","dat","osax","xcodeproj","swift","entitlements","xcassets","colorset","hmap","yaml","dep","h","c","cpp","d","dia","xib","lproj","m","strings","build","pbindex","o","linkfilelist")
+
+foreach($item in $sourceFileTypeArray) {
+
+     #deal with the jpeg/jpeg issue
+     if($item -eq "jpg") {
+          $fileTypeItem = @([fileType]::new("jpeg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("jpg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "jpeg") {
+          $fileTypeItem = @([fileType]::new("jpeg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("jpg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     #now other cases where the app is the file, so word for docx, powerpoint for pptx, photoshop for psd, etc.
+     #we are not going to try to decode stuff like "adobe"
+
+     if($item -eq "doc") {
+          $fileTypeItem = @([fileType]::new("doc",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("word",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "docx") {
+          $fileTypeItem = @([fileType]::new("docx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("word",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "xls") {
+          $fileTypeItem = @([fileType]::new("xls",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("excel",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "xlsx") {
+          $fileTypeItem = @([fileType]::new("xlsx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("excel",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "ppt") {
+          $fileTypeItem = @([fileType]::new("ppt",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("powerpoint",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "pptx") {
+          $fileTypeItem = @([fileType]::new("pptx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("powerpoint",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+     
+     if($item -eq "pdf") {
+          $fileTypeItem = @([fileType]::new("pdf",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("acrobat",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+     
+     if($item -eq "psd") {
+          $fileTypeItem = @([fileType]::new("psd",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("photoshop",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "indd") {
+          $fileTypeItem = @([fileType]::new("indd",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("indesign",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "ai") {
+          $fileTypeItem = @([fileType]::new("ai",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("illustrator",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     #everything else just uses the type as the name
+     $fileTypeItem = @([fileType]::new($item,$item))
+     $fileTypeArrayList.Add($fileTypeItem)
+}
+$fileTypeArrayList
--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-
+<# 
 $darwinVersionSplitOptions = [System.StringSplitOptions]::RemoveEmptyEntries
 $testResults = "choose file default location (`"$testPathString`" as POSIX file) with multiple selections allowed"|/usr/bin/osascript -so
 
@@ -17,4 +17,120 @@ foreach($entry in $testArray) {
      } else {
           write-output "$entry has no comma"
      }
+} #>
+
+class fileType {
+     [string]$name
+     [string]$type
+
+     fileType(
+          [string]$name,
+          [string]$type
+     ){
+          $this.name = $name
+          $this.type = $type
+     }
 }
+
+#arrayList to hold the filetypes
+[System.Collections.ArrayList]$fileTypeArrayList=@()
+
+#$fileTypeItem = @([fileType]::new($name,$type))
+
+#the order of the array is somewhat important, dealing with common special cases first
+$sourceFileTypeArray = @("jpg","jpeg","doc","docx","xls","xlsx","ppt","pptx","pdf","psd","indd","ai","gif","png","mpeg","mp3","mp4","m4a","aiff","heic","pages","key","keynote","numbers","epub","ibooks","rtf","applescript","scpt","scptd","script","sh","py","pl","ps1","url","zip","app","pxm","der","p7c","pem","crt","cer","txt","text","vcf","ics","html","htm","sql","webloc","plist","workflow","lz4","json","csv","tsv","sqlite","dat","osax","xcodeproj","swift","entitlements","xcassets","colorset","hmap","yaml","dep","h","c","cpp","d","dia","xib","lproj","m","strings","build","pbindex","o","linkfilelist")
+
+foreach($item in $sourceFileTypeArray) {
+
+     #deal with the jpeg/jpeg issue
+     if($item -eq "jpg") {
+          $fileTypeItem = @([fileType]::new("jpeg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("jpg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "jpeg") {
+          $fileTypeItem = @([fileType]::new("jpeg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("jpg",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     #now other cases where the app is the file, so word for docx, powerpoint for pptx, photoshop for psd, etc.
+
+     if($item -eq "doc") {
+          $fileTypeItem = @([fileType]::new("doc",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("word",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "docx") {
+          $fileTypeItem = @([fileType]::new("docx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("word",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "xls") {
+          $fileTypeItem = @([fileType]::new("xls",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("excel",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "xlsx") {
+          $fileTypeItem = @([fileType]::new("xlsx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("excel",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "ppt") {
+          $fileTypeItem = @([fileType]::new("ppt",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("powerpoint",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "pptx") {
+          $fileTypeItem = @([fileType]::new("pptx",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("powerpoint",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+     
+     if($item -eq "pdf") {
+          $fileTypeItem = @([fileType]::new("pdf",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("acrobat",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+     
+     if($item -eq "psd") {
+          $fileTypeItem = @([fileType]::new("psd",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("photoshop",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "indd") {
+          $fileTypeItem = @([fileType]::new("indd",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("indesign",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     if($item -eq "ai") {
+          $fileTypeItem = @([fileType]::new("ai",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+          $fileTypeItem = @([fileType]::new("illustrator",$item))
+          $fileTypeArrayList.Add($fileTypeItem)
+     }
+
+     #everything else just uses the type as the name
+     $fileTypeItem = @([fileType]::new($item,$item))
+     $fileTypeArrayList.Add($fileTypeItem)
+}
+$fileTypeArrayList
